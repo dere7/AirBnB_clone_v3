bb958f91e988ba6dc8c95a8339dbfde75611e011
Dereje Desta <ddesta777@gmail.com>
2022-04-17 18:19:23 +0300

    created all REST actions for places and reviews

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,12 @@
 """starts a Flask web application"""
 from os import getenv
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
@@ -23,5 +25,5 @@ def not_found(err):
 
 if __name__ == '__main__':
     app.run(getenv('HBNB_API_HOST') or '0.0.0.0',
-            getenv('HBNB_API_PORT') or 500,
+            getenv('HBNB_API_PORT') or 5000,
             threaded=True)
